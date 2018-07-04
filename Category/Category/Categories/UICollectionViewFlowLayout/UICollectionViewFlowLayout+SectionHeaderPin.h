b21820984d39f46bc9c4cacc70adfba6550cc561
zhoujun <zhoujun@knowbox.cn>
2018-07-04 10:42:37 +0800

    collectionview sectionheader pin

---
+++ Category/Category/Categories/UICollectionViewFlowLayout/UICollectionViewFlowLayout+SectionHeaderPin.h
@@ -0,0 +1,16 @@
+//
+//  UICollectionViewFlowLayout+SectionHeaderPin.h
+//  Category
+//
+//  Created by Jun Zhou on 2018/7/4.
+//  Copyright © 2018年 Jun Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UICollectionViewFlowLayout (SectionHeaderPin)
+
+/**头部悬浮效果 default no 不悬浮*/
+@property (assign , nonatomic) BOOL sectionHeadersPinToVisibleBoundsAll;
+
+@end
